24c5aa410083a0e21d750d83d619918523e9045c
pieas-asif <pieas.asif4@gmail.com>
2021-06-22 19:24:51 +0600

    Reset to GIT

--- clickandrides/settings.py
+++ clickandrides/settings.py
@@ -79,13 +79,12 @@ WSGI_APPLICATION = 'clickandrides.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # 'NAME': config("DB_NAME"),
-        # 'USER': config("DB_USER"),
-        # 'PASSWORD': config("DB_PASSWORD"),
-        # 'HOST': config("DB_HOST"),
-        # 'PORT': config("DB_PORT"),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config("DB_NAME"),
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_PASSWORD"),
+        'HOST': config("DB_HOST"),
+        'PORT': config("DB_PORT"),
         # 'OPTIONS': {
         #     'read_default_file': Path(BASE_DIR, 'clickandrides/my.cnf'),
         # },
